cf66b6ff6362a2c81eebbae1ab6f3504257d177e
Mykola Kizan <mkizan@gmail.com>
2021-08-09 08:36:28 +0300

    delete comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import { Fragment, useState } from 'react';
 import Statistics from './components/Statistics';
 import FeedbackOptions from './components/FeedbackOptions';
 import Notification from './components/Notification';
@@ -75,61 +75,3 @@ export default function App() {
     </Fragment>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-
-//   countTotalFeedback = () => {
-//     return this.state.good + this.state.neutral + this.state.bad;
-//   };
-
-//   countPositiveFeedbackPercentage = () => {
-//     return (
-//       Math.round(
-//         100 /
-//           ((this.state.good + this.state.neutral + this.state.bad) /
-//             this.state.good),
-//       ) + '%'
-//     );
-//   };
-
-//   onLeaveFeedback = option => {
-//     this.setState(prevState => ({
-//       [option]: prevState[option] + 1,
-//     }));
-//   };
-
-//   render() {
-//     const { good, neutral, bad } = this.state;
-
-//     return (
-//       <Fragment>
-//         <Section title={'Please leave feedback'}>
-//           <FeedbackOptions
-//             options={['good', 'neutral', 'bad']}
-//             onLeaveFeedback={this.onLeaveFeedback}
-//           />
-//         </Section>
-//         <Section title={'Statistics'}>
-//           {this.countTotalFeedback() > 0 ? (
-//             <Statistics
-//               good={good}
-//               neutral={neutral}
-//               bad={bad}
-//               total={this.countTotalFeedback()}
-//               positivePercentage={this.countPositiveFeedbackPercentage()}
-//             />
-//           ) : (
-//             <Notification message={'No feedback given'} />
-//           )}
-//         </Section>
-//       </Fragment>
-//     );
-//   }
-// }
-
-// export default App;
